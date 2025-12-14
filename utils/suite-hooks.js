df618878d9cb3374939621c84c64dca3b4757edf
ZaidAR49 <148114243+ZaidAR49@users.noreply.github.com>
2025-12-14 13:02:38 +0300

    time

--- utils/suite-hooks.js
+++ utils/suite-hooks.js
@@ -3,6 +3,7 @@ import { app } from "../config/capabilities.js";
 export async function afterSuite(driver) {
   console.log("Test completed,Bye!");
   console.log("Ending session, closing app ...");
+  console.log(`test ended at: ${new Date().toISOString()}`);
   // await driver.terminateApp(app);
   // await driver.deleteSession();
 }
